48c7b8625f7e71ccd212d68e9cb67c41913ed40d
kiyota <kiyota@tagbangers.co.jp>
2022-11-19 21:06:35 +0900

    init commit

---
+++ src/test/kotlin/com/example/householdExpenses/HouseholdExpensesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.householdExpenses
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HouseholdExpensesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
