b520a7ff8dbf6f1226b649a40d3dca37ab7a7165
kiyota <kiyota@tagbangers.co.jp>
2022-11-23 01:06:51 +0900

    #14 add hello world

---
+++ src/main/kotlin/com/example/householdExpenses/presentation/api/expense/HelloWorldController.kt
@@ -0,0 +1,16 @@
+package com.example.householdExpenses.presentation.api.expense
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * @author kiyota
+ */
+@RestController
+@RequestMapping("/api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun helloWorld(): String = "hello world"
+}
