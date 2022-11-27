044471d4baf75b3a9fe84ee13c1a7f966b290ca6
kiyota <kiyota@tagbangers.co.jp>
2022-11-27 17:38:18 +0900

    #15 polish exec query

--- src/main/kotlin/com/example/householdExpenses/infrastructure/repository/jooq/ExpenseJooqRepository.kt
+++ src/main/kotlin/com/example/householdExpenses/infrastructure/repository/jooq/ExpenseJooqRepository.kt
@@ -4,6 +4,7 @@ import com.example.householdExpenses.domain.category.Category
 import com.example.householdExpenses.domain.expense.Expense
 import com.example.householdExpenses.domain.expense.ExpenseRepository
 import com.example.householdExpenses.jooq.codegen.keys.EXPENSES__FK_EXPENSE_CATEGORY_CD0468A2
+import com.example.householdExpenses.jooq.codegen.tables.Expenses
 import com.example.householdExpenses.jooq.codegen.tables.records.CategoriesRecord
 import com.example.householdExpenses.jooq.codegen.tables.records.ExpensesRecord
 import com.example.householdExpenses.jooq.codegen.tables.references.CATEGORIES
@@ -19,29 +20,32 @@ import org.springframework.stereotype.Repository
 class ExpenseJooqRepository(private val create: DSLContext) : ExpenseRepository {
 
     override fun getExpenses(): List<Expense> {
+        val e: Expenses = EXPENSES.`as`("e")
+        val c = CATEGORIES.`as`("c")
+
         val records: Result<Record> = create.selectFrom(
-            EXPENSES.leftOuterJoin(CATEGORIES)
-                .on(EXPENSES.CATEGORY_ID.eq(CATEGORIES.ID))
+            e.leftOuterJoin(c)
+                .on(e.CATEGORY_ID.eq(c.ID))
         )
-            .orderBy(EXPENSES.ID)
+            .orderBy(e.ID)
             .fetch()
 
         val expenses = records.map {
             val category = Category.reconstruct(
-                id = it.getValue(CATEGORIES.ID)!!,
-                name = it.getValue(CATEGORIES.NAME)!!,
-                rank = it.getValue(CATEGORIES.RANK)!!
+                id = it.getValue(c.ID)!!,
+                name = it.getValue(c.NAME)!!,
+                rank = it.getValue(c.RANK)!!
             )
             Expense.reconstruct(
-                id = it.getValue(EXPENSES.ID)!!,
+                id = it.getValue(e.ID)!!,
                 category = category,
-                member_id = it.getValue(EXPENSES.MEMBER_ID)!!,
-                name = it.getValue(EXPENSES.NAME)!!,
-                price = it.getValue(EXPENSES.PRICE)!!,
-                memo = it.getValue(EXPENSES.MEMO),
-                date = it.getValue(EXPENSES.DATE)!!,
-                repeatable_month = it.getValue(EXPENSES.REPEATABLE_MONTH)!!,
-                repeatable_count = it.getValue(EXPENSES.REPEATABLE_COUNT)!!
+                member_id = it.getValue(e.MEMBER_ID)!!,
+                name = it.getValue(e.NAME)!!,
+                price = it.getValue(e.PRICE)!!,
+                memo = it.getValue(e.MEMO),
+                date = it.getValue(e.DATE)!!,
+                repeatable_month = it.getValue(e.REPEATABLE_MONTH)!!,
+                repeatable_count = it.getValue(e.REPEATABLE_COUNT)!!
             )
         }
             .toList()
--- src/test/kotlin/com/example/householdExpenses/infrastructure/repository/jooq/ExpenseJooqRepositoryTests.kt
+++ src/test/kotlin/com/example/householdExpenses/infrastructure/repository/jooq/ExpenseJooqRepositoryTests.kt
@@ -1,5 +1,6 @@
 package com.example.householdExpenses.infrastructure.repository.jooq
 
+import com.example.householdExpenses.domain.Fixtures
 import com.example.householdExpenses.domain.expense.Expense
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Assertions.*
@@ -24,7 +25,8 @@ internal class ExpenseJooqRepositoryTests (@Autowired sut: ExpenseJooqRepository
     fun selectAll() {
         val expenses: List<Expense> = sut.getExpenses()
         assertThat(expenses).hasSize(3)
-        assertThat(expenses[0].name).isEqualTo("粉ミルク")
+        assertThat(expenses[0].name).isEqualTo(Fixtures.ExpenseA().name)
+        assertThat(expenses[0].category.name).isEqualTo(Fixtures.ExpenseA().category.name)
     }
 
     @Configuration
